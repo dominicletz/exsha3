930b8cd65c9c3f8cfd0fec001f762f65bf6ad481
Dominic Letz <dominicletz@exosite.com>
2019-04-26 18:27:01 +0800

    publishing updates

--- benchmark.exs
+++ benchmark.exs
@@ -2,8 +2,8 @@ long_string = String.duplicate("1234567890", 2048)
 short_string = String.duplicate("1", 128)
 
 Benchee.run(%{
-  "long  elx_sha3_256"    => fn -> ExSha3.sha3_256(long_string) end,
+  "long   ex_sha3_256" => fn -> ExSha3.sha3_256(long_string) end,
   "long  nif_sha3_256" => fn -> :sha3.hash(256, long_string) end,
-  "short elx_sha3_256"    => fn -> ExSha3.sha3_256(short_string) end,
+  "short  ex_sha3_256" => fn -> ExSha3.sha3_256(short_string) end,
   "short nif_sha3_256" => fn -> :sha3.hash(256, short_string) end,
 })
--- mix.exs
+++ mix.exs
@@ -3,19 +3,32 @@ defmodule ExSha3.MixProject do
 
   def project do
     [
-      app: :exsha3,
+      app: :ex_sha3,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
       # Docs
-      name: "ExSha3",
+      package: package(),
+      description: "ExSha3 is a pure Elixir implementation of Sha3 and the original Keccak1600-f",
       source_url: "https://github.com/dominicletz/exsha3",
       docs: [
         main: "ExSha3",
         extras: ["README.md"]
       ]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache 2.0"],
+      maintainers: [
+        "Dominic Letz"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/dominicletz/exsha3"
+      }
     ]
   end
 
